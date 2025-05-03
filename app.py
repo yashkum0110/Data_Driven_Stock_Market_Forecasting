1fb21075cb426f42936c70d308897fe60c9d3c32
Yashwanth_Kumar_R <yashkum0110@gmail.com>
2025-05-03 21:31:24 +0530

    Stock Symbol

--- app.py
+++ app.py
@@ -8,3 +8,12 @@ from plotly import graph_objs as go
 
 start = "2015-01-01"
 Today = date.today().strftime("%Y-%m-%d")
+
+st.title("Stock Forecast App")
+
+stocks =st.text_input('Enter Stock Symbol', 'GOOG')
+selected_stock = st.selectbox('Selected dataset for prediction', stocks)
+
+n_years = st.slider('Years of prediction:', 1, 4)
+period = n_years * 365
+
