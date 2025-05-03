e23c32ac25236082024c9d3860c54d058c3742ed
Yashwanth_Kumar_R <yashkum0110@gmail.com>
2025-05-03 21:36:04 +0530

    Dropping Multindexing data

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
 
-start = "2015-01-01"
-Today = date.today().strftime("%Y-%m-%d")
+START = "2015-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
 
 st.title("Stock Forecast App")
 
@@ -17,3 +17,24 @@ selected_stock = st.selectbox('Selected dataset for prediction', stocks)
 n_years = st.slider('Years of prediction:', 1, 4)
 period = n_years * 365
 
+@st.cache_data
+def load_data(ticker):
+    data = yf.download(ticker, START, TODAY)
+    data.reset_index(inplace=True)
+    return data
+
+data_load_state = st.text('Loading data...')
+
+try:
+    data = load_data(selected_stock)
+    if isinstance(data.columns, pd.MultiIndex):
+        data.columns = data.columns.droplevel(1) 
+        data = data.rename(columns={'':'Date'}) 
+    data_load_state.text('Loading data... done!')
+
+    st.subheader('Raw data')
+    st.write(data.tail())
+
+except Exception as e:
+    st.error(f"An error occurred: {e}")
+    st.error("Check if the stock ticker is valid and data is available.")    
